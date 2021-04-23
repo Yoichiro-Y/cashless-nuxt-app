aeb6928e40dd80ce190f5dbac2a3fda9707defd0
山本陽一郎 <yoichiro@MacBook-Pro-2.local>
2021-04-23 21:32:36 +0900

    トップページを編集

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,9 @@
         <div class="flex bg-white" style="height:600px;">
           <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
               <div>
-                  <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl"><span class="text-indigo-600">オトコ</span>のための<span class="text-indigo-600">コスメ</span>情報サイト</h2>
-                  <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl"><span class="text-indigo-600">OTOCOS</span> へようこそ</h2>
-                  <p class="mt-2 text-sm text-gray-500 md:text-base">「コスメ」という言葉の響きから、女性が使用するような化粧品を思い浮かべる方もいるかもしれません。<br>一般的に「メンズコスメ」とは、洗顔や保湿などのスキンケアやヘアケアを中心としたアイテムのことを指します。ビジネスシーンなどあらゆるシーンにおいて好印象を与えるためには清潔感が必要だったり、冬になると乾燥によるカサツキなどのトラブルを防ぐために保湿が欠かせなかったり...<br>以前よりもスキンケアを意識する男性が増えているんです。</p>
+                  <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl text-left"><span class="text-indigo-600">オトコ</span>のための<span class="text-indigo-600">コスメ</span>情報サイト</h2>
+                  <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl text-left"><span class="text-indigo-600">OTOCOS</span> へようこそ</h2>
+                  <p class="mt-2 text-sm text-gray-500 md:text-base text-left">「コスメ」という言葉の響きから、女性が使用するような化粧品を思い浮かべる方もいるかもしれません。<br>一般的に「メンズコスメ」とは、洗顔や保湿などのスキンケアやヘアケアを中心としたアイテムのことを指します。ビジネスシーンなどあらゆるシーンにおいて好印象を与えるためには清潔感が必要だったり、冬になると乾燥によるカサツキなどのトラブルを防ぐために保湿が欠かせなかったり...<br>以前よりもスキンケアを意識する男性が増えているんです。</p>
                   <div class="flex justify-center lg:justify-start mt-6">
                       <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">新規登録</a>
                       <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">ログイン</a>
@@ -19,6 +19,54 @@
               <div class="h-full object-cover bg">
                   <div class="h-full bg-black opacity-25"></div>
               </div>
+          </div>
+        </div>
+        <div class="container mx-auto">
+          <div class="flex">
+            <div class="w-full lg:w-8/12l">
+              <div class="mt-10 p-4 border-b-4 border-t-4 rounded-sm">
+                <h2 class="font-bold text-xl text-gray-800 text-center">人気のコスメランキング</h2>
+              </div>
+              <div class="flex mt-10">
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">スキンケア</h3>
+                </div>
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">ヘアケア</h3>
+                </div>
+              </div>
+              <div class="flex mt-7">
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">ボディケア</h3>
+                </div>
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">メイクアップ</h3>
+                </div>
+              </div>
+              <div class="mt-10 p-4 border-b-4 border-t-4 rounded-sm">
+                <h2 class="font-bold text-xl text-gray-800 text-center">新着記事</h2>
+              </div>
+              <div class="flex mt-10">
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">スキンケア</h3>
+                </div>
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">ヘアケア</h3>
+                </div>
+              </div>
+              <div class="flex mt-7">
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">ボディケア</h3>
+                </div>
+                <div class="flex-1 border-l-4 p-3">
+                  <h3 class="font-bold text-xl text-gray-800">メイクアップ</h3>
+                </div>
+              </div>
+            </div>
+            
+            <div class="w-0 lg:w-4/12">
+              
+            </div>
           </div>
         </div>
       </main>
--- pages/signup.vue
+++ pages/signup.vue
@@ -48,9 +48,9 @@
 	import Header from "@/components/Header.vue";
     import Footer from "@/components/Footer.vue";
     export default {
-    components: {
-                Header,
-                Footer,
-    }
+        components: {
+                    Header,
+                    Footer,
+        }
     }
 </script>
